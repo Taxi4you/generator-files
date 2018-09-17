40bc21171abf8c44a93698de75afcf0c0f9072d9
Taxi4you <41004207+Taxi4you@users.noreply.github.com>
2018-09-17 16:13:49 +0300

    Update demo.js

--- component/demo.js
+++ component/demo.js
@@ -1,16 +1,9 @@
-app.directive("demo", () => {
+app.directive("demo", ($rootScope) => {
   return {
     templateUrl: "/templateUrlPath",
     scope: true,
     link: function(scope, element, attrs) {
-      demo(scope, element, attrs)
+      
     }
   }
 });
-
-function demo(scope, element, attrs) {
-  
-  $scope = scope;
-  
-  // demo controller closure
-}
