9a83f89bcec6b4702ef4c8855dd060d55fdd9ec7
Taxi4you <41004207+Taxi4you@users.noreply.github.com>
2018-11-28 14:18:24 +0200

    Update demo.js

--- component/demo.js
+++ component/demo.js
@@ -1,23 +1,8 @@
-app.directive("demo", ($rootScope, parseService) => {
+app.directive("demo", ($rootScope) => {
   return {
     templateUrl: "/templateUrlPath",
-    scope: true,
+    scope: {},
     link: function($scope, element, attrs) {
-      
-      $scope.key = attrs.key;
-      $scope.selector = attrs.selector;
-      $scope.selectorNoDots = $scope.selector.replace(/[.]/g, "");
-      
-      if ($scope.defaultValue) $scope.value = $rootScope.getKeyValue($scope[$scope.key], $scope.selector);
-      if (attrs.value) $scope.value = attrs.value;
-      $scope.disabledItem = parseService.parse(attrs.disabledItem);
-     
-      $(document).on('keyup', '.' + $scope.selectorNoDots, function() {
-        $rootScope.setKeyValue($scope[$scope.key], $scope.selector, $(this).val());
-        $rootScope.setKeyValue($rootScope.errors, $scope.selector, null);
-        $scope.$apply();
-        $rootScope.$digest();
-      });
       
     }
   }
