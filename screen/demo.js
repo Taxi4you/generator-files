e540cad8fc8b094a53fe8196764f5a16e8e947ac
Taxi4you <41004207+Taxi4you@users.noreply.github.com>
2018-12-12 14:31:51 +0200

    Update demo.js

--- screen/demo.js
+++ screen/demo.js
@@ -6,22 +6,6 @@ app.config(function($routeProvider, $locationProvider) {
   // $locationProvider.html5Mode(true);
 });
 
-app.controller("demo", ($rootScope, $scope, $location, $routeParams) => {
-
-  $rootScope.init().then(function() {
-    if ($rootScope.user.type!="guest")
-      $scope.screenInit();
-    else
-     $location.path("/auth/login");
-    $scope.$apply();
-  });
-
-  $scope.screenInit = function() {
-    $scope.initialized = true;
-    $rootScope.active_level_1 = null;
-    $rootScope.active_sub_1 = null;
-    $rootScope.active_sub_2 = null;
-    $scope.$apply();
-  };
+app.controller("demo", ($rootScope, $scope) => {
 
 });
